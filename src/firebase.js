dfad9402f74af353057facba5cf69da82218784e
AYMEN <moniakamli95@gmail.com>
2021-03-05 14:06:30 +0100

    add Real-time database with Firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBsLDEI3Hc_Hgrl4SWBtABWoMDFHdpNzYU",
+    authDomain: "twitter-clonee-27f24.firebaseapp.com",
+    projectId: "twitter-clonee-27f24",
+    storageBucket: "twitter-clonee-27f24.appspot.com",
+    messagingSenderId: "822731517506",
+    appId: "1:822731517506:web:386d2a085b1a908744c7db",
+    measurementId: "G-1SYS19BSRY"
+};
+
+const firebase = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
